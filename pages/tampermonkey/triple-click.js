bc121023ab7482a9c0c822e69a122e6b565fd3ed
Colin Chung <colin.d.chung@gmail.com>
2021-11-18 12:20:52 -0500

    trp click btn

--- pages/tampermonkey/triple-click.js
+++ pages/tampermonkey/triple-click.js
@@ -1,22 +1,42 @@
 import Link from "next/link";
+import { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
-const TripleClick = () => (
-  <div className="app-container">
-    <h1>TRIPLE CLICK TO COPY</h1>
-    <div style={{ width: '100%'}}>
-      <h2>Triple click password to copy it to clipboard!</h2>
-      <p>Meeting ID: www.someurl.zoom.com</p>
-      <p>Password: A7hj0129Pn!</p>
+function TripleClick(){
+
+  const [val, setVal] = useState("");
+
+  return (
+    <div className="app-container">
+      <h1>TRIPLE CLICK TO COPY</h1>
+      <div style={{ width: '100%'}}>
+        <h2>Triple click password to copy it to clipboard!</h2>
+        <p>Meeting ID: www.someurl.zoom.com</p>
+        <p>Password: A7hj0129Pn!</p>
+      </div>
+      <div className="trpclk-input-container">
+        <TextField 
+          variant="outlined"
+          label="Try pasting here!" 
+          placeholder="Password"
+          value={val}
+          onChange={e => setVal(e && e.target ? e.target.value : "")}
+        />
+
+        <div className="button">
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => setVal("")}
+          >
+            Clear
+          </Button>
+        </div>
+      </div>
     </div>
-    <div style={{ width: '100%', paddingTop: '2em'}}>
-      <TextField 
-        variant="outlined"
-        label="Try pasting here!" 
-        placeholder="Password"
-      />
-    </div>
-  </div>
-);
+  );
+}
+
 
 export default TripleClick;
